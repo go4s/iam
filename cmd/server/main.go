344e477d761c00a790f2650ea9c6205d011ebadd
zhangli2946 <zhangli2946@gmail.com>
2026-04-22 02:40:23 +0800

    更新端口

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -55,7 +55,7 @@ func main() {
         }
     }
     
-    if err := r.Run(":8080"); err != nil {
+    if err := r.Run(); err != nil {
         log.Fatalf("Failed to start server: %v", err)
     }
 }
